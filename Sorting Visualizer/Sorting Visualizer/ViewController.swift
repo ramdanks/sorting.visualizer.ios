b61954b070a9d73648f49996eb98467adf3dd725
ramdanks <ramadhan.kalih@ui.ac.id>
2022-05-31 16:37:43 +0700

    logic bubble sort only

--- Sorting Visualizer/Sorting Visualizer/ViewController.swift
+++ Sorting Visualizer/Sorting Visualizer/ViewController.swift
@@ -7,13 +7,143 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    var viewModel: ViewModel!
+    
+    var graphViews: Array<UIView> = []
+    let dataSizeMax = 20
+    let dataSizeMin = 6
+    
+    var cancellable: AnyObject!
+    var cancellable2: AnyObject!
+    var cancellable3: AnyObject!
+    var cancellable4: AnyObject!
+    
+    @IBOutlet weak var dataSizeSlider: UISlider!
+    @IBOutlet weak var graphView: UIView!
+    @IBOutlet weak var randomizeButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var orderLabel: UILabel!
+    @IBOutlet weak var orderSegmented: UISegmentedControl!
+    
+    let neutralColor: UIColor = .systemBlue
+    let accessColor: UIColor = .systemRed
+    let swapColor: UIColor = .systemYellow
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.viewModel = ViewModel(dataSize: dataSizeMin)
+        self.dataSizeSlider.value = 0.0
+        
+        cancellable4 = self.viewModel.$buttonInteractionEnable.sink(receiveValue: { enable in
+            DispatchQueue.main.async { [unowned self] in
+                dataSizeSlider.isEnabled = enable
+                randomizeButton.isEnabled = enable
+                orderSegmented.isEnabled = enable
+                playButton.isEnabled = enable
+            }
+        })
+        cancellable = self.viewModel.$dataSourceToggleChanged.sink(receiveValue: { [unowned self] _ in draw() })
+        cancellable2 = self.viewModel.$dataIndexAccessed.sink(receiveValue: { [unowned self] in
+            // restore previously accessed data to unhighlighted
+            setGraphBarColor(viewModel.dataIndexAccessed, color: neutralColor)
+            setGraphBarColor($0, color: accessColor)
+        })
+        cancellable3 = self.viewModel.$dataIndexSwapped.sink(receiveValue: { [unowned self] in
+            if ($0.isEmpty) { return }
+            setGraphBarColor($0, color: swapColor)
+            let view0 = graphViews[$0[0]]
+            let view1 = graphViews[$0[1]]
+            DispatchQueue.main.async {
+                let view0Height = view0.frame.height
+                let view1Height = view1.frame.height
+                view0.constraints.first(where: { $0.firstAttribute == .height })?.constant = view1Height
+                view1.constraints.first(where: { $0.firstAttribute == .height })?.constant = view0Height
+            }
+        })
     }
 
-
+    
+    private func draw()
+    {
+        let barSpacing: CGFloat = 8.0
+        var prevBar: UIView?
+        
+        graphView.subviews.forEach { $0.removeFromSuperview() }
+        graphViews.removeAll()
+        
+        for height in viewModel.dataSource
+        {
+            // most left (leading) bar
+            let leadingBar = prevBar == nil
+                
+            let bar = UIView()
+            graphViews.append(bar)
+            graphView.addSubview(bar)
+            bar.backgroundColor = .systemBlue
+            
+            bar.layer.cornerRadius = 60 / CGFloat(viewModel.dataSource.count)
+            bar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            
+            let topInset = 16.0
+            let barHeight = (graphView.frame.height - topInset) * CGFloat(height) / CGFloat(viewModel.dataSize)
+            let leadingAnchor = leadingBar ? graphView.leadingAnchor : prevBar!.trailingAnchor
+            
+            bar.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                bar.heightAnchor.constraint(equalToConstant: barHeight),
+                bar.bottomAnchor.constraint(equalTo: graphView.bottomAnchor),
+                bar.leadingAnchor.constraint(equalTo: leadingAnchor, constant: barSpacing),
+            ])
+            prevBar?.widthAnchor.constraint(equalTo: bar.widthAnchor).isActive = true
+            prevBar = bar
+        }
+        prevBar?.trailingAnchor.constraint(equalTo: graphView.trailingAnchor, constant: -barSpacing).isActive = true
+    }
+    
+    private func setGraphBarColor(_ range: Array<Int>, color: UIColor)
+    {
+        DispatchQueue.main.async { [unowned self] in
+            for i in range {
+                graphViews[i].backgroundColor = color
+            }
+        }
+    }
+    
+    @IBAction func onThemeChanged(_ sender: UISegmentedControl)
+    {
+        let style: UIUserInterfaceStyle = sender.selectedSegmentIndex == 0 ? .light : .dark
+        self.overrideUserInterfaceStyle = style
+        self.setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    @IBAction func onDataSizeChanged(_ sender: UISlider)
+    {
+        let range = dataSizeMax - dataSizeMin
+        self.viewModel.dataSize = dataSizeMin + Int(sender.value * Float(range))
+    }
+    
+    @IBAction func onOrderChanged(_ sender: UISegmentedControl)
+    {
+        setGraphBarColor([Int](0..<graphViews.count), color: neutralColor)
+        self.viewModel.orderAscending = sender.selectedSegmentIndex == 0
+        let orderText = sender.selectedSegmentIndex == 0 ? "Ascending" : "Descending"
+        orderLabel.text = "Order: \(orderText)"
+    }
+    
+    @IBAction func onRandomizeButton(_ sender: UIButton)
+    {
+        self.viewModel.randomize()
+    }
+    
+    @IBAction func onPlayButton(_ sender: UIButton)
+    {
+        DispatchQueue.global(qos: .userInteractive).async { [unowned self] in
+            self.viewModel.sort()
+            DispatchQueue.main.async { [unowned self] in graphViews.forEach { $0.backgroundColor = .systemGreen } }
+        }
+    }
 }
-
--- Sorting Visualizer/Sorting Visualizer/ViewModel.swift
+++ Sorting Visualizer/Sorting Visualizer/ViewModel.swift
@@ -9,5 +9,79 @@ import Foundation
 
 class ViewModel
 {
+    public var dataSize: Int { didSet {
+        if (oldValue != dataSize) { generate() }
+    }}
     
+    public var orderAscending: Bool = true
+    
+    private(set) var dataSource: Array<Int> = []
+    
+    @Published
+    private(set) var dataSourceToggleChanged: Bool = false
+    
+    @Published
+    private(set) var dataIndexAccessed: Array<Int> = []
+    
+    @Published
+    private(set) var dataIndexSwapped: Array<Int> = []
+    
+    @Published
+    private(set) var buttonInteractionEnable: Bool = true
+    
+    public init(dataSize: Int)
+    {
+        self.dataSize = dataSize
+        generate()
+    }
+    
+    public func randomize()
+    {
+        for _ in 1...dataSize
+        {
+            let i1 = Int(arc4random()) % dataSize
+            let i2 = Int(arc4random()) % dataSize
+            dataSource.swapAt(i1, i2)
+        }
+        dataSourceToggleChanged = !dataSourceToggleChanged
+    }
+    
+    public func sort()
+    {
+        buttonInteractionEnable = false
+        let sleepTimeMicro = useconds_t(1000000 / dataSize)
+        
+        for i in 0..<dataSize
+        {
+            for j in i+1..<dataSize
+            {
+                dataIndexAccessed = [i, j]
+                usleep(sleepTimeMicro)
+                if
+                (
+                    orderAscending && dataSource[i] > dataSource[j] ||
+                    !orderAscending && dataSource[i] < dataSource[j]
+                )
+                {
+                    dataIndexSwapped = [i, j]
+                    let temp = dataSource[i]
+                    dataSource[i] = dataSource[j]
+                    dataSource[j] = temp
+                    usleep(sleepTimeMicro)
+                }
+            }
+        }
+        
+        dataIndexAccessed = []
+        dataIndexSwapped = []
+        buttonInteractionEnable = true
+    }
+    
+    private func generate()
+    {
+        dataSource = []
+        dataSource.reserveCapacity(dataSize)
+        dataSource.append(contentsOf: 1...dataSize)
+        dataSourceToggleChanged = !dataSourceToggleChanged
+    }
 }
